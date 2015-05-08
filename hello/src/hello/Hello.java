8d779e323e0f5186050951a96733fe4145c4115d
취업반_PM <_PM@ITbank011-PC>
2015-05-08 19:44:44 +0900

    launching..

---
+++ hello/src/hello/Hello.java
@@ -0,0 +1,30 @@
+package hello;
+
+class Hello {
+ public static void main(String[] args) { 
+  for (int i = 2; i < 10; i+=4) {
+   for (int j = 1; j < 10; j++) {
+    System.out.println(
+      i+"x"+j+"="+(i*j)+"\\t"
+      +(i+1)+"x"+j+"="+((i+1)*j)+"\\t"
+      +(i+2)+"x"+j+"="+((i+2)*j)+"\\t"
+      +(i+3)+"x"+j+"="+((i+3)*j)+"\\t");
+   } System.out.println();
+  }    
+  System.out.println("---------------------------------\\n");
+  
+  for (int j = 1; j < 10; j++) {
+   for (int i = 2; i < 6; i++) {
+    System.out.print(i + "*" + j + "=" + i * j + "\\t");
+   }
+   System.out.println();
+  }
+  System.out.println();
+  for (int j = 1; j < 10; j++) {
+   for (int i = 6; i < 10; i++) {
+    System.out.print(i + "*" + j + "=" + i * j + "\\t");
+   }
+   System.out.println();
+  }
+ }
+}
